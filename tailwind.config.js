bea5961718fbc97a9a09b80c9aeb673b19858c2d
Bassal <javi@altabert.com>
2022-12-27 00:27:42 +0100

    Add tailwind footer and tailwind config settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+      175: "1.75rem",
+    },
     colors: {
       transparent: "transparent",
       current: "currentColor",
       black: "#030303",
       white: "#f3f3f3",
       grey: "#c7c5c5",
-      greyDark: "",
+      greyDark: "#dee2e6",
       orange: "#ee7b0f",
       orangeActive: "#f6b06e",
       blue: "#0d6efd",
       blueActive: "#0b5ed7",
     },
     screens: {
+      md: "768px",
       lg: "992px",
       xl: "1200px",
       xxl: "1400px",
@@ -34,6 +45,7 @@ module.exports = {
       },
       spacing: {
         18: "18px",
+        312: ".3125rem",
       },
     },
   },
