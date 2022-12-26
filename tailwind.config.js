65a7d329d98a33ea22d982e639cefb31c0ba1ed0
Bassal <javi@altabert.com>
2022-12-26 21:19:46 +0100

    Transform card component partial to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,40 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#030303",
+      white: "#f3f3f3",
+      grey: "#c7c5c5",
+      greyDark: "",
+      orange: "#ee7b0f",
+      orangeActive: "#f6b06e",
+      blue: "#0d6efd",
+      blueActive: "#0b5ed7",
+    },
+    screens: {
+      lg: "992px",
+      xl: "1200px",
+      xxl: "1400px",
+    },
+    container: {
+      padding: "0.75rem",
+      screens: {
+        md: "720px",
+        lg: "960px",
+        xl: "1140px",
+        xxl: "1320px",
+      },
+    },
+    extend: {
+      flexBasis: {
+        300: "300px",
+      },
+      spacing: {
+        18: "18px",
+      },
+    },
   },
   plugins: [],
 };
